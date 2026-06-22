c238a1b308552a8eec0734cf1a18791b0778cb51
Shubham-Gedam <shubhgedam28@gmail.com>
2026-06-22 21:47:59 +0530

    fix db error handling

--- temple_Service/src/db/db.js
+++ temple_Service/src/db/db.js
@@ -4,10 +4,10 @@ import mongoose from 'mongoose';
 async function connectDB(){
     try{
         await mongoose.connect(process.env.MONGO_URI),
-        console.log("connected to tempe db");
+        console.log("connected to temple db");
         
     }catch(err){
-        console.log("Database Failed", error);
+        console.log("Database Failed", err);
         
     }
 }
